a5834051088a94edb3bcc4ff2123d24ae99a1ec1
tamzin22 <evelynetamzin@gmail.com>
2021-11-19 04:21:20 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="container">
+   <Header />
+ </div>
+   
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+  *{
+    box-sizing : border-box;
+    margin  : 0 ;
+    padding : 0 ;
+  }
+
+   body{
+     font-family : 'Poppins' , sans-serif;
+     
+   }
+
+   .container{
+     max-width : 500px;
+     margin: 30px auto;
+     overflow: auto;
+     min-height: 300px;
+     border: 1px solid steelblue;
+     padding: 30px;
+     border-radius: 30px;
+   }
+   
+   .btn{
+     display: inline-block;
+     background: #000;
+     color: #fff;
+     border: none;
+     padding: 10px 20px;
+     margin: 5px;
+     border-radius: 5px;
+     cursor: pointer;
+     text-decoration: none;
+     font-size: 15px;
+     font-family: inherit;
+   }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>Tack Tracker</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style  scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+
+    }
+</style>
